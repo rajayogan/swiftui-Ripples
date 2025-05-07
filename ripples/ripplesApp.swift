2e3eafe623cf7a0e0977705ff2244258a87d6a5e
Rajayogan <rajayogan27@gmail.com>
2025-05-07 22:18:06 +0530

    Initial Commit

---
+++ ripples/ripplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ripplesApp.swift
+//  ripples
+//
+//  Created by Rajayogan on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ripplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ripplesTests/ripplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ripplesTests.swift
+//  ripplesTests
+//
+//  Created by Rajayogan on 07/05/25.
+//
+
+import Testing
+@testable import ripples
+
+struct ripplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
